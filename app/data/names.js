75327762f8e9f48deb9e4e55fc2cc4774f98bc2a
David <david.carter.developer@gmail.com>
2017-01-18 10:51:51 +0000

    new names

--- app/data/names.js
+++ app/data/names.js
@@ -18,6 +18,36 @@ const names = [
 		id: 4,
 		name: 'Helene Husebo',
 		emoji: ':peach: :peach:'
+	},
+	{
+		id: 5,
+		name: 'Rob Sloan',
+		emoji: ':french_bread: :french_bread:'
+	},
+	{
+		id: 6,
+		name: 'Anindita Ghosh',
+		emoji: ''
+	},
+	{
+		id: 7,
+		name: 'Millar Maxwell',
+		emoji: ':heart_eyes: :bicyclist:'
+	},
+	{
+		id: 8,
+		name: 'Sam Billington',
+		emoji: ':musical_score: :hearts: :flag_eu:'
+	},
+	{
+		id: 9,
+		name: 'Freddie Yauner',
+		emoji: ':ramen: :hearts: :art:'
+	},
+	{
+		id: 10,
+		name: 'Shift.ms',
+		emoji: ':rainbow: :rainbow: :rainbow:'
 	}
 
 ];
