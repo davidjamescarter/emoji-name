d81b5665ac5666c3921e93da41b61936f318ac74
David <david.carter.developer@gmail.com>
2017-01-19 15:04:54 +0000

    New names

--- app/data/names.js
+++ app/data/names.js
@@ -6,47 +6,97 @@ const names = [
 	},
 	{
 		id: 2,
+		name: 'donald trump',
+		emoji: ':100: :poop:'
+	},
+	{
+		id: 3,
 		name: 'Theresa May',
 		emoji: ':flag_eu: :snake:'
 	},
 	{
-		id: 3,
+		id: 4,
+		name: 'theresa may',
+		emoji: ':flag_eu: :snake:'
+	},
+	{
+		id: 5,
 		name: 'Helene Husebø',
 		emoji: ':peach: :peach:'
 	},
 	{
-		id: 4,
+		id: 6,
 		name: 'Helene Husebo',
 		emoji: ':peach: :peach:'
 	},
 	{
-		id: 5,
+		id: 7,
 		name: 'Rob Sloan',
 		emoji: ':french_bread: :french_bread:'
 	},
 	{
-		id: 6,
-		name: 'Anindita Ghosh',
-		emoji: ''
+		id: 8,
+		name: 'rob sloan',
+		emoji: ':french_bread: :french_bread:'
 	},
 	{
-		id: 7,
+		id: 9,
+		name: 'Anindita Ghosh',
+		emoji: ':dancers: :muscle: :punch:'
+	},
+	{
+		id: 10,
+		name: 'anindita ghosh',
+		emoji: ':dancers: :muscle: :punch:'
+	},
+	{
+		id: 11,
 		name: 'Millar Maxwell',
 		emoji: ':heart_eyes: :bicyclist:'
 	},
 	{
-		id: 8,
+		id: 12,
+		name: 'millar maxwell',
+		emoji: ':heart_eyes: :bicyclist:'
+	},
+	{
+		id: 13,
 		name: 'Sam Billington',
 		emoji: ':musical_score: :hearts: :flag_eu:'
 	},
 	{
-		id: 9,
+		id: 14,
+		name: 'sam billington',
+		emoji: ':musical_score: :hearts: :flag_eu:'
+	},
+	{
+		id: 15,
 		name: 'Freddie Yauner',
 		emoji: ':ramen: :hearts: :art:'
 	},
 	{
-		id: 10,
+		id: 16,
+		name: 'freddie yauner',
+		emoji: ':ramen: :hearts: :art:'
+	},
+	{
+		id: 17,
 		name: 'Shift.ms',
+		emoji: ':rainbow: :rainbow: :rainbow:'
+	},
+	{
+		id: 18,
+		name: 'shift.ms',
+		emoji: ':rainbow: :rainbow: :rainbow:'
+	},
+	{
+		id: 19,
+		name: 'makerversity',
+		emoji: ':rainbow: :rainbow: :rainbow:'
+	},
+	{
+		id: 20,
+		name: 'makerversity',
 		emoji: ':rainbow: :rainbow: :rainbow:'
 	}
 
